7afd96fb1a49fc58efea85be30ee49c042855783
Ben <bensterl15@gmail.com>
2022-09-15 03:07:08 -0400

    Accidently kept Sijin's branch

--- SegmentationSkeloton/segmentation_processing/_3DUnet/train.py
+++ SegmentationSkeloton/segmentation_processing/_3DUnet/train.py
@@ -31,23 +31,6 @@ zarr_path = os.path.join(data_dir, zarr_name)
 print(zarr_path)
 log_dir = 'logs'
 
-num_fmaps = 256
-input_shape = gp.Coordinate((12, 128, 128))
-output_shape = gp.Coordinate((2, 88, 88))
-
-batch_size = 2
-
-voxel_size = gp.Coordinate((1, 1, 1))
-input_size = input_shape * voxel_size
-output_size = output_shape * voxel_size
-
-checkpoint_every = 200
-train_until = 2000
-snapshot_every = 200
-zarr_snapshot = False
-
-num_workers = 16
-cache_size = 100
 
 
 class CustomLoss(torch.nn.Module):
@@ -57,37 +40,30 @@ class CustomLoss(torch.nn.Module):
     def forward(self, inputs, targets, smooth=1):
         inputs = inputs.view(-1)
         targets = targets.view(-1)
-
-        # intersection = (inputs * targets).sum()
-        # dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)
-
         BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
-
-        # Compute number of pixels wrongly selected as 0 (accepting null hypothesis)
-        # targets_inv = -1*(targets - 1)
-        # type2_error = (inputs * targets_inv).sum()
-
-        Total_Loss = BCE  # + type2_error/(327680)
-
-        return Total_Loss
+        return BCE
 
 
-def mknet():
+def mknet(pdict):
+    num_fmaps = pdict['num_fmaps']
+    fmap_inc_factor = pdict['fmap_inc_factor']
+    downsample_factors = pdict['downsample_factors']
+    kernel_size_down = pdict['kernel_size_down']
+    kernel_size_up = pdict['kernel_size_up']
+    in_channels = pdict['in_channels']
     unet = UNet(
-        in_channels=1,
+        in_channels=in_channels,
         num_fmaps=num_fmaps,
-        fmap_inc_factor=2,
-        downsample_factors=[
-            [1, 2, 2],
-            [1, 2, 2],
-            # [1, 3, 3],
-        ],
-        kernel_size_down=[[[2, 3, 3], [2, 3, 3]]] * 3,
-        kernel_size_up=[[[2, 3, 3], [2, 3, 3]]] * 2,
+        fmap_inc_factor=fmap_inc_factor,
+        downsample_factors=downsample_factors,
+        kernel_size_down=kernel_size_down,
+        kernel_size_up=kernel_size_up,
     )
 
+    conv_in_channels = pdict['conv_in_channels']
+    conv_kernel = pdict['conv_kernel']
     unet = nn.DataParallel(unet)
-    convpass = ConvPass(num_fmaps, 1, [[1, 1, 1]], activation='Sigmoid')
+    convpass = ConvPass(num_fmaps, in_channels, conv_kernel, activation='Sigmoid')
     convpass = nn.DataParallel(convpass)
 
     model = torch.nn.Sequential(
@@ -97,11 +73,21 @@ def mknet():
     return (model)
 
 
-def train(iterations):
+def train(pdict):
+    batch_size = pdict['batch_size']
+    snapshot_every = pdict['snapshot_every']
+    voxel_size = gp.Coordinate(pdict['voxel_size'])
+    input_shape = gp.Coordinate(pdict['input_shape'])
+    output_shape = gp.Coordinate(pdict['output_shape'])
+
+    input_size = input_shape * voxel_size
+    output_size = output_shape * voxel_size
+
+    iterations = pdict['train_until']
+    learning_rate = pdict['learning_rate']
     model = mknet()
-    #	loss = CustomLoss() #torch.nn.BCELoss()
     loss = torch.nn.BCELoss()
-    optimizer = torch.optim.Adam(lr=5e-5, params=model.parameters())
+    optimizer = torch.optim.Adam(lr=learning_rate, params=model.parameters())
 
     raw = gp.ArrayKey('raw')
     gt = gp.ArrayKey('gt')
@@ -139,8 +125,6 @@ def train(iterations):
 
     pipeline += gp.Stack(batch_size)
 
-    # pipeline += gp.PreCache(num_workers = num_workers, cache_size = cache_size)
-
     pipeline += gp.Normalize(gt, factor=1)
 
     pipeline += Train(
@@ -169,20 +153,13 @@ def train(iterations):
         for i in tqdm(range(iterations)):
             pipeline.request_batch(request)
 
-
-print(f"__name__ is {__name__}")
-
+'''
 if __name__ == '__main__':
     if 'test' in sys.argv:
         train_until = 10
         snapshot_every = 1
-        num_workers = 1
 
     train_until = 5
     snapshot_every = 1
-    num_workers = 1
-
     train(train_until)
-
-if __name__ == "SegmentationSkeloton.segmentation_processing._3DUnet.train":
-    train(1)
+'''
--- Views/Frames/TrainingFrame.py
+++ Views/Frames/TrainingFrame.py
@@ -17,26 +17,40 @@ class TrainingFrame(ttk.Frame):
     def __create_widgets(self):
         options = {'padx': 5, 'pady': 5}
 
+        self.path_params = tk.StringVar()
+        self.params_button = ttk.Entry(self, textvariable=self.path_params, width=20)
+        self.params_button.grid(column=1, row=0, **options)
+        self.params_button.focus()
+
+        # convert button
+        params_open_button = ttk.Button(
+            self,
+            text='Select raw data folder',
+            command=self.load_params
+        )
+        params_open_button.grid(column=2, row=0, sticky='W', **options)
+
+
         # convert button
         training_button = ttk.Button(
             self,
             text='Train',
             command=self.process
         )
-        training_button.grid(column=1, row=0, sticky='W', **options)
+        training_button.grid(column=1, row=2, sticky='W', **options)
 
         stop_button = ttk.Button(
             self,
             text='Stop',
             command=self.stop
         )
-        stop_button.grid(column=1, row=0, **options)
+        stop_button.grid(column=1, row=2, **options)
 
         # self.text = Text(self, height=8, width= 50)
         # self.text.grid(column=1, row=1, sticky='W', **options)
 
         self.textFrame = MainTextFrame(self)
-        self.textFrame.grid(column=1, row=1, sticky='W', **options)
+        self.textFrame.grid(column=1, row=3, sticky='W', **options)
 
     def process(self):
         self.textFrame.insert('Runing train.py ...')
@@ -47,6 +61,11 @@ class TrainingFrame(ttk.Frame):
         from SegmentationSkeloton.segmentation_processing._3DUnet import train
 
         self.textFrame.insert( 'Done')
+
+    def load_params(self):
+        self.path_params = askdirectory(title='Select saved model parameters')
+        #self.rawDataPath = path
+        #self.set_raw_data_path(path)
 
     def stop(self):
         exit()
